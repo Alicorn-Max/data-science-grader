d794e7a9ec2c682d2fea7ca94658ae8a90de721f
Max Saraf <max.saraf@bush.edu>
2026-09-17 11:27:51 -0700

    Update grader.py
    Made grader work with the updated data.

--- grader.py
+++ grader.py
@@ -1,37 +1,50 @@
 import ast 
 
 def true_false_check(test_check, vars):
-    if eval(test_check[0].strip(),vars) == bool(test_check[1].replace("true","True").replace("false","False")):
+    if eval(test_check[0][-1].strip(),vars) == bool(test_check[1][0]):
         print("Correct!")
     else:
         print("Whoops, there's a mistake.")
-        print(test_check[0])
         return
     
 def float_check(test_check, vars):
-    if eval(test_check[0].strip(),vars) == float(test_check[1]):
+    if eval(test_check[0][-1].strip(),vars) == float(test_check[1][0]):
         print("Correct!")
     else:
         print("whoops, try again.")
-        print(test_check[0])
         return
 
+def table_check(test_check, vars):
+    if str(eval(test_check[0][-1].strip(),vars)) == test_check[1].join("\n"):
+        print("Correct!")
+    else:
+        print("whoops, try again.")
+        return
 
 def grade_question(question, vars):
-    with open("/content/data_science_grader/tests.txt", "r") as tests:
+    with open("tests.txt", "r") as tests: #CHANGE BACK TO /content/data_science_grader/tests.txt
         for test in tests.readlines():
             test = ast.literal_eval(test)
-            if test[0] == question:
+            identifier = test[0].split('-')
+            print(identifier)
+            if identifier[0] == vars['assignment'] and identifier[1] == question:
                 test_check = test[1]
-                if len(test_check) > 2:
-                    for code in test_check[:-2]:
+                if len(test_check[0]) > 1:
+                    for code in test_check[0]:
                         exec(code, vars)
-                    if "true" in test_check[-1] or "false" in test_check[0][-1]:
-                        true_false_check(test_check[-2:], vars)
+                    if len(test_check[1]) > 1:
+                        table_check(test_check, vars)
+                    elif "True" in test_check[1][0] or "False" in test_check[1][0]:
+                        true_false_check(test_check, vars)
                     else:
-                        float_check(test_check[-2:], vars)
-                elif "true"  in test_check[1] or "false" in test_check[1]:
+                        float_check(test_check, vars)
+                elif len(test_check[1]) > 1:
+                    table_check(test_check, vars)
+                elif "True" in test_check[1][0] or "False" in test_check[1][0]:
                     true_false_check(test_check, vars)
                 else:
                     float_check(test_check, vars)
-                        
+
+assignment = 'hw02'                  
+third_element = -6
+grade_question('q1_1', vars())
